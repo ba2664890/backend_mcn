bf64d9de92e91f32841e3c1c2efa54b746f7f29c
Abdou BA <ba2664890@gmail.com>
2025-10-08 21:01:20 +0000

    Finalisation03

--- museum_api/settings.py
+++ museum_api/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.railway.app', 'localhost']
 
 
 # Application definition
