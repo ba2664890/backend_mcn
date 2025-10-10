701657cfe592566ec449c15b531afa8c8a4c8fa1
Abdou BA <ba2664890@gmail.com>
2025-10-10 07:02:57 +0000

    Fix duplicate corsheaders app

--- museum_api/views.py
+++ museum_api/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
-from django.views.decorators.csrf import ensure_csrf_cookie
+from django.middleware.csrf import get_token
 
-@ensure_csrf_cookie
-def get_csrf_token(request):
-    return JsonResponse({'detail': 'CSRF cookie set'})
+def csrf_token(request):
+    token = get_token(request)
+    return JsonResponse({'csrfToken': token})
